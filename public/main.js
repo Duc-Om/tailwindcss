9a140736e23f084ae23671b1fae0df8beed860d5
Duc-Om <bduc210320@gmail.com>
2023-04-25 00:24:37 +0700

    [#1]upload
    template1

---
+++ public/main.js
@@ -0,0 +1,9 @@
+function mobileMenu(){
+    const btn_menu = document.getElementById('btn-menu-top');
+    const btn_toggel_icon_menu_top = document.getElementById('toggel-icon-menu-top');
+    btn_toggel_icon_menu_top.addEventListener('click', (e)=>{
+        console('aa'+e)
+    })
+        
+    
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ["./public/**/*.{html,js}",
+  "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
