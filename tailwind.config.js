dd309a5c7e3d69fa90dda5c81c333fa12612e3c4
Duc-Om <bduc210320@gmail.com>
2023-04-25 10:59:01 +0700

    resposive header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
   content: ["./public/**/*.{html,js}",
   "./src/**/*.{js,jsx,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'doctor': "url('./image/doctor-blue.png')",
+      },
+      keyframes: {
+        slideDown: {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        slideDown: 'slideDown .4s ease-in-out',
+      }
+    },
   },
   plugins: [],
 }
